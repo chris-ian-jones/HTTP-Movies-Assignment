b60c7990b67f3b4faa14b4dfeb0d06a8a8260716
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-22 22:35:22 +0100

    Completed MVP: now able to update and display the stars of a movie dependant on their id

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -15,12 +15,20 @@ const UpdateMovieForm = props => {
   console.log('UpdateMovieForm movieId: ', movieId)
   console.log('UpdateMovieForm props: ', props)
 
-  const [updatedMovieData, setUpdatedMovieDate] = useState({
+  const [currentMovieData, setCurrentMovieDate] = useState({
     id: '',
     title: '',
     director: '',
     metascore: '',
     stars: []
+  })
+
+  const [updatedMovieData, setUpdatedMovieDate] = useState({
+    id: '',
+    title: '',
+    director: '',
+    metascore: '',
+    stars: ''
   })
   
   useEffect(() => {
@@ -28,6 +36,7 @@ const UpdateMovieForm = props => {
       .get(`http://localhost:5000/api/movies/${movieId}`)
       .then(result => {
         console.log("✅ axios 'get' by movie id: ", result.data)
+        setCurrentMovieDate(result.data)
         setUpdatedMovieDate(result.data)
       })
       .catch(error => console.log("❌ axios 'get' by movie id: ", error))
@@ -44,8 +53,15 @@ const UpdateMovieForm = props => {
 
   const onSubmitHandler = event => {
     event.preventDefault()
+    const tempUpdatedMovieData = {
+      id: updatedMovieData.id,
+      title: updatedMovieData.title,
+      director: updatedMovieData.director,
+      metascore: updatedMovieData.metascore,
+      stars: updatedMovieData.stars.split(",")
+    }
     axios
-    .put(`http://localhost:5000/api/movies/${movieId}`, updatedMovieData)
+    .put(`http://localhost:5000/api/movies/${movieId}`, tempUpdatedMovieData)
     .then(result => {
       console.log("✅ axios 'put by movie id: ", result)
       setUpdatedMovieDate({
@@ -61,18 +77,37 @@ const UpdateMovieForm = props => {
   }
 
   return (
-      <StyledForm onSubmit={onSubmitHandler}>
-        <label>Title</label>
-        <input name='title' placeholder={updatedMovieData.title} value={updatedMovieData.title} onChange={onChangeHandler}/>
-        <label>Director</label>
-        <input name='director' placeholder={updatedMovieData.director} value={updatedMovieData.director} onChange={onChangeHandler} />
-        <label>Metascore</label>
-        <input name='metascore' placeholder={updatedMovieData.metascore} value={updatedMovieData.metascore} onChange={onChangeHandler} />
-        {/* Todo stars array input */}
-        {/* <label>Stars</label> */}
-        {/* <input name='stars' placeholder={updatedMovieData.stars} value={updatedMovieData.title} /> */}
-        <button onClick={onSubmitHandler}>Update</button>
-      </StyledForm>
+    <>
+      <div className="movie-card">
+        <h2>{currentMovieData.title}</h2>
+        <div className="movie-director">
+          Director: <em>{currentMovieData.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{currentMovieData.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {currentMovieData.stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="movie-card">
+        <StyledForm onSubmit={onSubmitHandler}>
+          <label>Title</label>
+          <input name='title' placeholder={updatedMovieData.title} value={updatedMovieData.title} onChange={onChangeHandler}/>
+          <label>Director</label>
+          <input name='director' placeholder={updatedMovieData.director} value={updatedMovieData.director} onChange={onChangeHandler} />
+          <label>Metascore</label>
+          <input name='metascore' placeholder={updatedMovieData.metascore} value={updatedMovieData.metascore} onChange={onChangeHandler} />
+          <label>Stars</label>
+          <input name='stars' placeholder={updatedMovieData.stars} value={updatedMovieData.stars} onChange={onChangeHandler} />
+          <button onClick={onSubmitHandler}>Update</button>
+        </StyledForm>
+      </div>
+    </>
   )
 }
 
